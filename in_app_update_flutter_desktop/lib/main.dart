4c7f2aa278d492967697000fb9fb73cc8357bcfa
Agnel Selvan Arunachalam <agnel.selvan@neosoftmail.com>
2022-03-23 13:09:49 +0530

    Added DMG File, Added Functionality

--- in_app_update_flutter_desktop/lib/main.dart
+++ in_app_update_flutter_desktop/lib/main.dart
@@ -1,10 +1,13 @@
 import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:in_app_update_flutter_desktop/application.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -15,6 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'In App Updates in Flutter Desktop App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -33,14 +37,93 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool isDownloading = false;
+  double downloadProgress = 0;
   Future<Map<String, dynamic>> loadJsonFromGithub() async {
-    final response = await http.read(Uri.parse("uri"));
+    final response = await http.read(Uri.parse(
+        "https://raw.githubusercontent.com/AgnelSelvan/Blogs/main/in_app_update_flutter_desktop/app_versions_check/version.json"));
     return jsonDecode(response);
   }
 
-  showUpdateDialog() {}
-  void _checkForUpdates() {
-    showUpdateDialog();
+  Future downloadNewVersion(String appPath) async {
+    final fileName = appPath.split("/").last;
+    isDownloading = true;
+    setState(() {});
+
+    final dio = Dio();
+    final fileDownloadPath =
+        "${(await getApplicationDocumentsDirectory()).path}/$fileName";
+    //https://github.com/AgnelSelvan/AgnelSelvan.github.io/blob/main/annai_store/annai-store.exe?raw=true
+    await dio.download(
+      "https://github.com/AgnelSelvan/Blogs/raw/main/in_app_update_flutter_desktop/app_versions_check/$appPath",
+      fileDownloadPath,
+      onReceiveProgress: (received, total) {
+        final progress = (received / total) * 100;
+        debugPrint('Rec: $received , Total: $total, $progress%');
+        downloadProgress = double.parse(progress.toStringAsFixed(0));
+        setState(() {});
+      },
+    );
+    // await openExeFile(fileDownloadPath);
+    isDownloading = false;
+    setState(() {});
+  }
+
+  showUpdateDialog(Map<String, dynamic> versionJson) {
+    final version = versionJson['version'];
+    final updates = versionJson['description'] as List;
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            contentPadding: const EdgeInsets.all(10),
+            title: Text("Latest Version $version"),
+            children: [
+              Text("What's new in $version"),
+              const SizedBox(
+                height: 5,
+              ),
+              ...updates
+                  .map((e) => Row(
+                        children: [
+                          Container(
+                            width: 4,
+                            height: 4,
+                            decoration: BoxDecoration(
+                                color: Colors.grey[400],
+                                borderRadius: BorderRadius.circular(20)),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "$e",
+                            style: TextStyle(
+                              color: Colors.grey[600],
+                            ),
+                          ),
+                        ],
+                      ))
+                  .toList(),
+              const SizedBox(
+                height: 10,
+              ),
+              if (version > ApplicationConfig.currentVersion)
+                TextButton.icon(
+                    onPressed: () {
+                      downloadNewVersion(versionJson["file_name"]);
+                    },
+                    icon: const Icon(Icons.update),
+                    label: const Text("Update")),
+            ],
+          );
+        });
+  }
+
+  Future<void> _checkForUpdates() async {
+    final jsonVal = await loadJsonFromGithub();
+    debugPrint("Response: $jsonVal");
+    showUpdateDialog(jsonVal);
   }
 
   @override
@@ -50,12 +133,25 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Current Version is ${ApplicationConfig.currentVersion}',
+        child: Stack(
+          children: [
+            Center(
+              child: Text(
+                'Current Version is ${ApplicationConfig.currentVersion}',
+              ),
             ),
+            if (isDownloading)
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                color: Colors.black.withOpacity(0.3),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    CircularProgressIndicator(),
+                  ],
+                ),
+              )
           ],
         ),
       ),
